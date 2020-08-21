a0357d1ef0d86616ff0136da8bc17cb902edb5fb
hat_lover <jobin25051999@gmail.com>
2020-08-21 22:33:29 +0530

    backend ok

--- pre_decisions_call.py
+++ pre_decisions_call.py
@@ -1,5 +1,5 @@
 from rounds_loop import*
-round_array = [round_1,round_2]
+round_array = [round_1,round_2,round_3,round_4,round_5,round_6,round_7,round_8,round_9,round_10,round_11,round_12,round_13,round_14,round_15,round_16]
 
 def rounding_loop(getchr,getmi,getma,getrno):
     for i in range(getmi-1,getma):
--- rounds_loop.py
+++ rounds_loop.py
@@ -7,3 +7,195 @@ def round_2(ind_rounds,get_chars):
         for b in range(ind_rounds):
             print(str(get_chars[a])+str(get_chars[b]))
 
+def round_3(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c]))
+
+def round_4(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d]))
+
+def round_5(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e]))
+
+def round_6(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f]))
+
+
+def round_7(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g]))
+
+
+def round_8(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h]))
+
+def round_9(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i]))
+
+
+def round_10(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j]))
+
+
+def round_11(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k]))
+
+
+def round_12(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                for l in range(ind_rounds):
+                                                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l]))
+
+
+
+def round_13(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                for l in range(ind_rounds):
+                                                    for m in range(ind_rounds):
+                                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m]))
+
+def round_14(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                for l in range(ind_rounds):
+                                                    for m in range(ind_rounds):
+                                                        for n in range(ind_rounds):
+                                                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n]))
+
+
+def round_15(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                for l in range(ind_rounds):
+                                                    for m in range(ind_rounds):
+                                                        for n in range(ind_rounds):
+                                                            for o in range(ind_rounds):
+                                                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o]))
+
+def round_16(ind_rounds,get_chars):
+    for a in range(ind_rounds):
+        for b in range(ind_rounds):
+            for c in range(ind_rounds):
+                for d in range(ind_rounds):
+                    for e in range(ind_rounds):
+                        for f in range(ind_rounds):
+                            for g in range(ind_rounds):
+                                for h in range(ind_rounds):
+                                    for i in range(ind_rounds):
+                                        for j in range(ind_rounds):
+                                            for k in range(ind_rounds):
+                                                for l in range(ind_rounds):
+                                                    for m in range(ind_rounds):
+                                                        for n in range(ind_rounds):
+                                                            for o in range(ind_rounds):
+                                                                for p in range(ind_rounds):
+                                                                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o])+str(get_chars[p]))
+
+
+
+
+
+
+
+
+
+                        
