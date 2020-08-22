9a33d54df00852f0a08427c11eeb2a20fd5ede43
hat_lover <jobin25051999@gmail.com>
2020-08-22 17:13:08 +0530

    backend everything ok

--- rounds_loop.py
+++ rounds_loop.py
@@ -1,44 +1,52 @@
+FILE = open("word_list.txt","a")
 def round_1(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for i in range(ind_rounds):
-        print(get_chars[i])
+        FILE.write(str(get_chars[i])+'\n')
         
 def round_2(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
-            print(str(get_chars[a])+str(get_chars[b]))
+            FILE.write(str(get_chars[a])+str(get_chars[b])+'\n')
 
 def round_3(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
-                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c]))
+                FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+'\n')
 
 def round_4(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
                 for d in range(ind_rounds):
-                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d]))
+                    FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+'\n')
 
 def round_5(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
                 for d in range(ind_rounds):
                     for e in range(ind_rounds):
-                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e]))
+                        FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+'\n')
 
 def round_6(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
                 for d in range(ind_rounds):
                     for e in range(ind_rounds):
                         for f in range(ind_rounds):
-                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f]))
+                            FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+'\n')
 
 
 def round_7(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -46,10 +54,11 @@ def round_7(ind_rounds,get_chars):
                     for e in range(ind_rounds):
                         for f in range(ind_rounds):
                             for g in range(ind_rounds):
-                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g]))
+                                FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+'\n')
 
 
 def round_8(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -58,9 +67,10 @@ def round_8(ind_rounds,get_chars):
                         for f in range(ind_rounds):
                             for g in range(ind_rounds):
                                 for h in range(ind_rounds):
-                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h]))
+                                        FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+'\n')
 
 def round_9(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -70,10 +80,11 @@ def round_9(ind_rounds,get_chars):
                             for g in range(ind_rounds):
                                 for h in range(ind_rounds):
                                     for i in range(ind_rounds):
-                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i]))
+                                        FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+'\n')
 
 
 def round_10(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -84,10 +95,11 @@ def round_10(ind_rounds,get_chars):
                                 for h in range(ind_rounds):
                                     for i in range(ind_rounds):
                                         for j in range(ind_rounds):
-                                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j]))
+                                            FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+'\n')
 
 
 def round_11(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -99,10 +111,11 @@ def round_11(ind_rounds,get_chars):
                                     for i in range(ind_rounds):
                                         for j in range(ind_rounds):
                                             for k in range(ind_rounds):
-                                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k]))
+                                                FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+'\n')
 
 
 def round_12(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -115,11 +128,12 @@ def round_12(ind_rounds,get_chars):
                                         for j in range(ind_rounds):
                                             for k in range(ind_rounds):
                                                 for l in range(ind_rounds):
-                                                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l]))
+                                                    FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+'\n')
 
 
 
 def round_13(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -133,9 +147,10 @@ def round_13(ind_rounds,get_chars):
                                             for k in range(ind_rounds):
                                                 for l in range(ind_rounds):
                                                     for m in range(ind_rounds):
-                                                        print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m]))
+                                                        FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+'\n')
 
 def round_14(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -150,10 +165,11 @@ def round_14(ind_rounds,get_chars):
                                                 for l in range(ind_rounds):
                                                     for m in range(ind_rounds):
                                                         for n in range(ind_rounds):
-                                                            print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n]))
+                                                            FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+'\n')
 
 
 def round_15(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
         for b in range(ind_rounds):
             for c in range(ind_rounds):
@@ -169,10 +185,12 @@ def round_15(ind_rounds,get_chars):
                                                     for m in range(ind_rounds):
                                                         for n in range(ind_rounds):
                                                             for o in range(ind_rounds):
-                                                                print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o]))
+                                                                FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o])+'\n')
 
 def round_16(ind_rounds,get_chars):
+    FILE = open("word_list.txt","a")
     for a in range(ind_rounds):
+        print (ind_rounds)
         for b in range(ind_rounds):
             for c in range(ind_rounds):
                 for d in range(ind_rounds):
@@ -188,9 +206,10 @@ def round_16(ind_rounds,get_chars):
                                                         for n in range(ind_rounds):
                                                             for o in range(ind_rounds):
                                                                 for p in range(ind_rounds):
-                                                                    print(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o])+str(get_chars[p]))
+                                                                    FILE.write(str(get_chars[a])+str(get_chars[b])+str(get_chars[c])+str(get_chars[d])+str(get_chars[e])+str(get_chars[f])+str(get_chars[g])+str(get_chars[h])+str(get_chars[i])+str(get_chars[j])+str(get_chars[k])+str(get_chars[l])+str(get_chars[m])+str(get_chars[n])+str(get_chars[o])+str(get_chars[p])+'\n')
 
-
+FILE.close()
+print ('DONE!')
 
 
 
